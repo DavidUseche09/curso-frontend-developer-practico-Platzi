76a2f31c9d0eab4eb45b85721507c3b2418dbbf1
JacktheRipper0911 <daviduseche09@gmail.com>
2023-11-20 21:19:43 -0500

    Conectando el cartLogo con el cuadro de compras

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const myOrderDesktop = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerIcon.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', showMyOrder);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -12,5 +15,9 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+function showMyOrder(){
+    myOrderDesktop.classList.toggle('inactive');
 }
 
